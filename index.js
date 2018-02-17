9647660575a3caa08aa91db115860a6525b7d703
Ben Slabbert <ben@hex.co.za>
2018-02-17 11:53:14 +0200

    adding nodemon for dev refactoring into separate packages

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+
+console.log('Setting up Passport');
+require('./services/passport');
+console.log('Done');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.CLIENT_ID,
-    clientSecret: keys.CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
-}, ( accessToken, refreshToken, profile, done ) => {
-    console.log('accessToken', accessToken);
-    console.log('refreshToken', refreshToken);
-    console.log('profile', profile);
-    console.log('done', done);
-}));
-
-app.get('/auth/google',
-    passport.authenticate('google', {
-        // the info we want from google
-        scope: [ 'profile', 'email' ]
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+console.log('Adding routes');
+require('./routes/authRoutes')(app);
+console.log('Done');
 
 const PORT = process.env.PORT || 5000;
 
