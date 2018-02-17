f95df99dc01a4aca7b22ab51c2828bb28dafd9f5
Ben Slabbert <ben@hex.co.za>
2018-02-17 11:23:20 +0200

    get user info from google

--- index.js
+++ index.js
@@ -9,10 +9,23 @@ passport.use(new GoogleStrategy({
     clientID: keys.CLIENT_ID,
     clientSecret: keys.CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
-}, accessToken => {
-    console.log(accessToken);
+}, ( accessToken, refreshToken, profile, done ) => {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+    console.log('done', done);
 }));
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        // the info we want from google
+        scope: [ 'profile', 'email' ]
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+console.log('Server running at: ' + PORT);
