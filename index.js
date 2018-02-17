30478297a44a587469d445476559c1ed049db8fe
Ben Slabbert <ben@hex.co.za>
2018-02-17 15:05:42 +0200

    wiring up auth flow enable cookies for session
    adding cookie-session

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('./logger/logger');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 let keys;
 if ( process.env.PROFILE === 'dev' ) {
     keys = require('./config/keys');
 }
 
+// declare the model before mongoose needs it
 require('./models/User');
 
 logger.info('Setting up Passport');
@@ -20,6 +23,16 @@ logger.info('Done');
 logger.info('Starting Express');
 const app = express();
 logger.info('Done');
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        keys: [ process.env.COOKIE_KEY || keys.cookieKey ] // randomly chooses one of these keys to encrypt
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 logger.info('Adding routes');
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const logger = require('../logger/logger');
+
+passport.serializeUser(( user, done ) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(( id, done ) => {
+    User.findById(id)
+        .then(user => done(null, user));
+});
 
 let keys;
 if ( process.env.PROFILE === 'dev' ) {
@@ -11,8 +23,21 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.CLIENT_SECRET || keys.CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
 }, ( accessToken, refreshToken, profile, done ) => {
-    console.log('accessToken', accessToken);
-    console.log('refreshToken', refreshToken);
-    console.log('profile', profile);
-    console.log('done', done);
+
+    let googleId = profile.id;
+    logger.info('Looking for user with googleId: ' + googleId);
+
+    User.findOne({ googleId })
+        .then(( existingUser ) => {
+                if ( existingUser ) {
+                    logger.info('User found');
+                    done(null, existingUser);
+                } else {
+                    logger.info('No user found, creating a new user');
+                    new User({ googleId })
+                        .save()
+                        .then(newUser => done(null, newUser));
+                }
+            }
+        );
 }));
