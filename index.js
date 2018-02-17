9c4207c29571f1d264014b912251f4c6eb81da06
Ben Slabbert <ben@hex.co.za>
2018-02-17 10:41:35 +0200

    adding oath stuff

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', ( req, res ) => {
-    res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+if ( !CLIENT_ID || !CLIENT_SECRET ) {
+    throw new Error("No OAuth Credentials Provided");
+}
+
 app.listen(PORT);
