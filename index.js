2aaee6a2e7bbfe9d9a002ea510386da1ebe0ca68
Ben Slabbert <ben@hex.co.za>
2018-02-17 10:45:09 +0200

    wiring up keys

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.CLIENT_ID,
+    clientSecret: keys.CLIENT_SECRET
+}));
 
 const PORT = process.env.PORT || 5000;
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
-if ( !CLIENT_ID || !CLIENT_SECRET ) {
-    throw new Error("No OAuth Credentials Provided");
-}
 
 app.listen(PORT);
