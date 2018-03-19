7f12877cb5fc2a93fb8d88109e55d0453519c501
Ben Slabbert <ben@hex.co.za>
2018-03-19 15:16:46 +0200

    adding button for stripe styles adding header and description for charge

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 class Payments extends Component {
     render() {
         return (
             <StripeCheckout
+                name="Email Survey"
+                description="$5 for 5 email credits"
                 amount={500}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_PK}
-            />
+            >
+                <button className="btn">
+                    Add Credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
