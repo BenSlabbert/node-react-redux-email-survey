890335b3838cbe6bab6fc533f03bcd94cd20a6fd
Ben Slabbert <ben@hex.co.za>
2018-03-19 07:56:53 +0200

    adding env variables

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     mongoURL: process.env.mongoURL,
-    cookieKey: process.env.cookieKey
+    cookieKey: process.env.cookieKey,
+    STRIPE_PK: process.env.STRIPE_PK,
+    STRIPE_SK: process.env.STRIPE_SK
 };
