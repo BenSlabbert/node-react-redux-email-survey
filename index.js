1a0c185ca299ddf656ab6bd573b6e794461967d9
Ben Slabbert <ben@hex.co.za>
2018-03-22 14:03:24 +0200

    adding prod configuration for requests between express and react

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 logger.info('Done');
 
+if (process.env.NODE_ENV === 'production') {
+    // serve react app
+    app.use(express.static('client/build'));
+
+    // send the index.html file if the request path is unknown
+    // serves as a 'catch all' for routes
+    const path = require('path');
+    app.get('*', ( req, res ) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
